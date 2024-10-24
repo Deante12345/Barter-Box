acba97f9f7427dba8a7f6fa2c4e592b0f94b348d
Khoi Truong <khoi.truong98@gmail.com>
2024-10-24 09:17:22 -0500

    added page update, i can open now

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main (page: ft.Page):
     def route_change (route) :
         page.views.clear()
         page.views.append(views_handler(page)[page.route])
-
+        page.update()
     page.on_route_change = route_change
 
     page.go("/signup")
