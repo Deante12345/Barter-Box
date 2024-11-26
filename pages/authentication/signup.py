38b1e463f01e0642ba23517b8cb99e411387f056
ntillm <ntillm3@lsu.edu>
2024-11-26 09:09:09 -0700

    successful signup and login with the database

--- pages/authentication/signup.py
+++ pages/authentication/signup.py
@@ -1,8 +1,11 @@
 import time
 import flet as ft
-import sqlite3
-from db import db_path
+import psycopg2
+from psycopg2.errors import UniqueViolation
+from db.connection import get_connection
+from db.queries import create_user
 from pages.authentication.crud import check_data_exists, insert_data
+from db.hash_password import hash_password
 from utils.validation import Validation
 
 class SignUp(ft.Container):
@@ -63,11 +66,9 @@ class SignUp(ft.Container):
                                 ft.Container(height=20),  # Space between inputs and button
                                 ft.ElevatedButton(text="Sign Up", on_click=self.signup),
                                 self.error_field,  # Display error messages
-                                ft.Text("Password Requirements: 1 capital and 1 lowercase letter and 1 digit 0-9")
-
                             ],
                         ),
-                ),
+                    ),
                 ),
             ]
         )
@@ -84,8 +85,9 @@ class SignUp(ft.Container):
 
         # Check if all fields are filled
         if first_name and last_name and email and username and password and re_password:
-            conn = sqlite3.connect(db_path)
-            
+            conn = get_connection()
+            cursor = conn.cursor()
+
             # Check for valid email
             if not self.validation.is_valid_email(email):
                 self.email_field.border = self.error_border
@@ -98,22 +100,7 @@ class SignUp(ft.Container):
                 self.error_field.size = 0
                 self.error_field.update()
                 self.email_field.update()
-
-            # Check for valid password
-            elif not valid_password(password):
-                self.password_field.border = self.error_border
-                self.error_field.value = "Invalid password"
-                self.error_field.size = 12
-                self.error_field.update()
-                self.password_field.update()
-                time.sleep(1)
-                self.password_field.border = self.default_border
-                self.error_field.size = 0
-                self.error_field.update()
-                self.password_field.update()
-
-            # Check if passwords match
-            elif password != re_password:
+            elif re_password != password:
                 self.re_password_field.border = self.error_border
                 self.error_field.value = "Passwords don't match"
                 self.error_field.size = 12
@@ -124,38 +111,38 @@ class SignUp(ft.Container):
                 self.error_field.size = 0
                 self.error_field.update()
                 self.re_password_field.update()
-            
-
-
-            # Check if email already exists
-            elif not check_data_exists(conn, "user", f"email='{email}'"):
-                # Insert the user data into the database
-                insert_data(conn, "user", (first_name, last_name, email, username, password))
-
-                # Show success message
-                self.page.splash = ft.ProgressBar()
-                self.error_field.value = "You have successfully been registered"
-                self.error_field.color = "green"
-                self.error_field.size = 12
-                self.error_field.update()
-                self.page.update()
-                time.sleep(1)
-                self.page.splash = None
-                self.page.update()
-                self.page.go("/login")
-
             else:
-                # If email exists
-                self.email_field.border = self.error_border
-                self.error_field.value = "Email already exists"
-                self.error_field.size = 12
-                self.error_field.update()
-                self.email_field.update()
-                time.sleep(1)
-                self.email_field.border = self.default_border
-                self.error_field.size = 0
-                self.error_field.update()
-                self.email_field.update()
+                try:
+
+                    #hash password before inserting in database
+                    hashed_password = hash_password(password)
+
+                    user_id = create_user(first_name, last_name, username, hashed_password, email)
+            # Check if email already exists
+                    self.page.splash = ft.ProgressBar()
+                    self.error_field.value = "You have successfully been registered"
+                    self.error_field.color = "green"
+                    self.error_field.size = 12
+                    self.error_field.update()
+                    self.page.update()
+                    time.sleep(1)
+                    self.page.splash = None
+                    self.page.update()
+                    self.page.go("/login")
+
+                except UniqueViolation:
+                    self.error_field.value = "Email or Username already exists"
+                    self.error_field.size = 12
+                    self.error_field.update()
+                    time.sleep(1)
+                    self.error_field.size = 0
+                    self.error_field.update()
+
+                except Exception as ex:
+                    print(f"Database error: {ex}")
+                    self.error_field.value = "An error occurred. Please try again."
+                    self.error_field.size = 12
+                    self.error_field.update()
 
         else:
             # If any field is empty
@@ -165,13 +152,6 @@ class SignUp(ft.Container):
             time.sleep(1)
             self.error_field.size = 0
             self.error_field.update()
-
-        # Debugging: Confirm the signup process
+    # Debugging: Confirm the signup process
         print("This is our signup")
         print(f"First Name: {first_name}, Last Name: {last_name}, Email: {email}")
-
-def valid_password(password):
-    uppercase_flag = password.lower() != password
-    lowercase_flag = password.upper() != password
-    contains_number = any(char.isdigit() for char in password)
-    return uppercase_flag and lowercase_flag and contains_number
