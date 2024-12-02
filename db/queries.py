c5b12d57b713691c2588ca4fd2b2016a2da35bf6
ntillm <ntillm3@lsu.edu>
2024-12-02 01:35:00 -0700

    pulling posts added

--- db/queries.py
+++ db/queries.py
@@ -31,3 +31,30 @@ def get_user_info(username):
          cursor.execute(query, (username,))
          return cursor.fetchone()
 
+def create_post(poster_id, title, description, quantity, points, zipcode, expiration_date):
+   query = """
+    INSERT INTO posts(poster_id, title, description, quantity, points, zipcode, expiration_date, image_url)
+    VALUES(%s, %s, %s, %s, %s, %s, %s) RETURNING poster_id;
+   """
+   with get_connection() as conn:
+      with conn.cursor() as cursor:
+         cursor.execute(query, (poster_id,title, description, quantity, points, zipcode, expiration_date, image_url))
+         return cursor.fetchone()[0]
+
+def get_all_posts():
+   query = """
+    SELECT post_id, title, description, quantity, points, zipcode, expiration_date, image_url
+    FROM posts;
+   """
+   with get_connection() as conn:
+      with conn.cursor as cursor:
+         cursor.execute(query)
+         return cursor.fetchall()
+def get_post_by_user(user_id):
+   query = """SELECT post_id, title, description, quantity, points, zipcode, expiration_date, image_url
+   FROM posts WHERE user_id = %;
+   """
+   with get_connection() as conn:
+      with conn.cursor as cursor:
+         cursor.execute(query)
+         return cursor.fetchall()
